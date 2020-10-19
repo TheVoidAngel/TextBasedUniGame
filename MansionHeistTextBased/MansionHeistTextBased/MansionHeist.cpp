ea4acfa35fcdd49422f9bb52f6f8731d097b3791
George Salsa <george.salsa@ontariotechu.net>
2020-10-19 16:31:16 -0400

    Note at beginning

--- MansionHeistTextBased/MansionHeistTextBased/MansionHeist.cpp
+++ MansionHeistTextBased/MansionHeistTextBased/MansionHeist.cpp
@@ -2,18 +2,29 @@
 using namespace std;
 
 // Hey guys this is a test
-int main() {
 
-}
 
-int dice() {
 
-}
 
-int timer() {
+int main()
+	{
+		cout << "You enter the main lobby of the mansion, you see a note on the floor, which says read me!" << endl;
+		cout << "The note reads: Welcome to Manson Hiest, where the only way to escape is to answer a bunch of riddles which reveal a secret word for you to use to escape!"
+			<< " May the odds be forever in your favor!" << endl;
+		
+	}
 
-}
+	int dice()
+	{
+		return 0;	
+	}
 
-int riddle() {
+	int timer()
+	{
+		return 0;
+	}
 
-}
+	int riddle()
+	{
+		return 0;
+	}
