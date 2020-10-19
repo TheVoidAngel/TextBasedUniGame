722dd0273ef6202795495b40613aa6effdd97537
Josh <nickyj528@gmail.com>
2020-10-19 16:38:11 -0400

    Character screen

--- MansionHeistTextBased/MansionHeistTextBased/MansionHeist.cpp
+++ MansionHeistTextBased/MansionHeistTextBased/MansionHeist.cpp
@@ -8,6 +8,10 @@ using namespace std;
 
 int main()
 	{
+		int player1;
+		cout << "Choose your character: " << endl << "1. Hacker: Haze (H4Z3) - Kevin Hayes" << endl << "2. Plumber: Duce Harmon" << endl << "3. Chef : Charlotte Hunter" << endl << "4. Bookworm : Vivienne Gray" << endl << "5. Bodybuilder : Jack Brooks" << endl << "6. Actor : Sebastian Moltif" << endl << "7. Normal : Ave Jones" << endl << "8. Survivalist : Vex Helana" << endl;
+		cin >> player1;
+
 		cout << "You enter the main lobby of the mansion, you see a note on the floor, which says read me!" << endl;
 		cout << "The note reads: Welcome to Manson Hiest, where the only way to escape is to answer a bunch of riddles which reveal a secret word for you to use to escape!"
 			<< " May the odds be forever in your favor!" << endl;
