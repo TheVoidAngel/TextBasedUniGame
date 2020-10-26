2e352fe584f48733d8f5c83a2c2c2a958c9039b7
xavier70000 <73137244+xavier70000@users.noreply.github.com>
2020-10-25 23:26:48 -0400

    Xavier's art

---
+++ MansionHeistTextBased/MansionHeistTextBased/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " ____________                                __   " << endl;
+	cout << "(	     )                              /  |         " << endl;
+	cout << "(	     )                               ||   " << endl;
+	cout << "(____________)______________________         ||	                                " << endl;
+	cout << "|				    |  ||||||||||||	   " << endl;
+	cout << "|				    |  ||||||||||||" << endl;
+	cout << "|				    |  ||	 ||" << endl;
+	cout << "|___________________________________|  ||	 ||" << endl;
+}
